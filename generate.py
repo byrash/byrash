97ecc5ca6e256cc67491561a58c1809aa938e972
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2020-08-09 15:44:58 +1000

    Add stats

--- generate.py
+++ generate.py
@@ -20,6 +20,14 @@ f.write(f'''
 - 💬 Ping me about **Architecture**, **Clean code**,  **Development**, **Design thinking**
 - 📫 Reach me: [shivaji.byrapaneni@gmail.com](mailto:shivaji.byrapaneni@gmail.com)
 - ⚡️ Fun fact: I'm a huge fan of Multi Threading (All the things that happens only in Prod ) & Security Concepts.
+
+
+## Github Usage ~ Status 
+![Shivaji's github stats](https://github-readme-stats.vercel.app/api?username=byrash&show_icons=true&hide_border=true&theme=dark&private=true)   
+
+## Language Usage ~ Status
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=byrash&layout=compact&theme=dark&show_icons=true&hide_border=true&private=true)
+
 </p>
 <h3 align="center">Have a nice day!</h3>
 ''')
